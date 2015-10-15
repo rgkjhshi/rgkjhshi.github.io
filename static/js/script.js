0bf77cf1f09bd806b7900a417b2e2af05e627444
Michael King <rgkjhshi@gmail.com>
2015-10-15 15:50:55 +0800

    deploy blog

---
+++ static/js/script.js
@@ -0,0 +1,27 @@
+/**
+ * 页面ready方法
+ */
+$(document).ready(function() {
+    backToTop();
+});
+
+ // 回到顶部
+function backToTop() {
+    var st = $(".page-scrollTop");
+    var $window = $(window);
+    //滚页面才显示返回顶部
+    $window.scroll(function() {
+        var topOffset = $window.scrollTop();
+        if (topOffset > 300) {
+            st.fadeIn(500);
+        } else {
+            st.fadeOut(500);
+        }
+    });
+    //点击回到顶部
+    st.click(function() {
+        $("body").animate({
+            scrollTop: "0"
+        }, 500);
+    });
+}
